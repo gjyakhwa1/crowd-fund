75b4e644199a112949569515696b03f41149704f
Gaurav Jyakhwa <gjyakhwa1@gmail.com>
2024-06-25 08:06:49 +0545

    :sparkles: Easy task 4 scripts

---
+++ scripts/userWithdrawFund.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat");
+
+const contractAddress = "";
+async function main() {
+  const CrowdTank = await ethers.getContractFactory("CrowdTank");
+  const crowdTank = await CrowdTank.attach(contractAddress);
+  const projectId = 1;
+  const txn = await crowdTank.userWithdrawFunds(projectId);
+  console.log("Txn Status",txn.hash)
+  console.log("Transaction",txn)
+}
+
+main()
+  .then(() => process.exit())
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
